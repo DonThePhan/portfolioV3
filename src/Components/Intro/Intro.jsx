7df37b7100e54a5ffba3bd1cc37990e32313133e
DonThePhan <donthephan@gmail.com>
2022-09-23 18:57:34 -0400

    right side near completion, missing blurred divs

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Intro.css';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 
-import Github from '../../img/github.png';
-import LinkedIn from '../../img/linkedin.png';
-import Instagram from '../../img/instagram.png';
+import github from '../../img/github.png';
+import linkedin from '../../img/linkedin.png';
+import instagram from '../../img/instagram.png';
+import vector1 from '../../img/Vector1.png';
+import vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+import thumbsup from '../../img/thumbup.png';
+import crown from '../../img/crown.png';
+import glassesimoji from '../../img/glassesimoji.png';
 
 const Intro = () => {
   return (
@@ -20,17 +27,32 @@ const Intro = () => {
         <button className='button i-button'>Hire me</button>
         <div className='i-icons'>
           <a href=''>
-            <img src={Github} alt='' />
+            <img src={github} alt='' />
           </a>
           <a href=''>
-            <img src={LinkedIn} alt='' />
+            <img src={linkedin} alt='' />
           </a>
           <a href=''>
-            <img src={Instagram} alt='' />
+            <img src={instagram} alt='' />
           </a>
         </div>
       </div>
-      <div className='i-right'>i am right side</div>
+      <div className='i-right'>
+        <img src={vector1} alt='' />
+        <img src={vector2} alt='' />
+        <img src={boy} alt='' />
+        <img src={glassesimoji} alt='' />
+        <div style={{ top: '-4%', left: '68%' }}>
+          <FloatingDiv image={crown} txt1='Web' txt2='Developer'></FloatingDiv>
+        </div>
+        <div style={{ top: '18rem', left: '0rem' }}>
+          <FloatingDiv
+            image={thumbsup}
+            txt1='Best Design'
+            txt2='Award'
+          ></FloatingDiv>
+        </div>
+      </div>
     </div>
   );
 };
